f251fc5b0745249fb7f1f680f75a32c73852470c
justapig9020 <justapig9020@gmail.com>
2023-06-11 21:39:02 +0800

    Add debug.h

--- ramulator/src/Cache.cpp
+++ ramulator/src/Cache.cpp
@@ -1,14 +1,8 @@
 #include "Cache.h"
 
-#ifndef DEBUG_CACHE
-#define debug(...)
-#else
-#define debug(...) do { \
-          printf("\033[36m[DEBUG] %s ", __FUNCTION__); \
-          printf(__VA_ARGS__); \
-          printf("\033[0m\n"); \
-      } while (0)
-#endif
+// #define DEBUG
+#include "debug.h"
+
 
 namespace ramulator
 {
---
+++ ramulator/src/debug.h
@@ -0,0 +1,14 @@
+#ifndef __DEBUG_H
+#define __DEBUG_H
+
+#ifndef DEBUG
+#define debug(...)
+#else
+#define debug(...) do { \
+          printf("\033[36m[DEBUG] %s ", __FUNCTION__); \
+          printf(__VA_ARGS__); \
+          printf("\033[0m\n"); \
+      } while (0)
+#endif
+
+#endif
